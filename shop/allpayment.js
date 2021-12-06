7d152a8102f32a38219e3c96f89e052e5ab1fa79
itzmearaa <88992728+Vall-Afk@users.noreply.github.com>
2021-12-06 17:28:40 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -2,31 +2,24 @@ const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
 
 *🏧 BANK*
+-
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ -
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ -
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ +6281270532125
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama -
 
 *📲 PULSA*
 
-Tidak tersedia
-	
+Telkomsel : +6281270532125
+
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
 	}
